078e9ae9ade897e8b3c714d2a2c99b168080f635
Fakhri17 <fakhricoffe@gmail.com>
2024-06-23 14:02:08 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+
+
+<template>
+  <RouterView />
+  <div>
+    <input v-model="name" type="text">
+    <p>{{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'John Doe'
+    }
+  }
+}
+</script>
